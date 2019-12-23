faeb59493bf18f28b5aa1c568d63a943e4f70b46
Daniel Laufenberg <danlanfen26@gmail.com>
2019-12-23 00:29:52 -1000

    API features added

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const searchURL = 'https://api.github.com/users/';
+
+function displayResults(responseJson, userName){
+    $('#results-list').empty();
+    $('#results-list').append(`
+    <tr>
+    <td><b>Project</b></th>
+    <td><b>URL</b></th>
+    </tr>`);
+    // if there are previous results, remove them
+    for (let i = 0; i < responseJson.length; i++){
+        $('#results-list').append(`
+        <tr>
+        <td>${responseJson[i].full_name.replace(`${userName + '/'}`, '')}</td>
+        <td>${responseJson[i].url}</td>
+        </tr>
+        `);
+    }
+
+    $('#results').removeClass('hidden');
+}
+
+function getNews(query){
+    const url = searchURL + query + '/repos';
+    fetch(url)
+      .then(response => {return response.json();})
+      .then(responseJson => displayResults(responseJson, query))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+}
+
+function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchTerm = $('#js-search-term').val();
+      getNews(searchTerm);
+    });
+}
+
+$(watchForm);
